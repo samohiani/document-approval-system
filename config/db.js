953bc071ca17c8bdf6cce7fca0f65be7f30b1574
AdelodunAbraham <ab.adelodun@gmail.com>
2025-05-17 19:02:59 +0100

    Refactor code structure for improved readability and maintainability

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const { DB_HOST, DB_USERNAME, PASSWORD, DB_NAME } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
-  port: 5433,
+  port: 5432,
   timezone: "+01:00",
   logging: (msg) => logFile.write(`${msg}\n`),
 });
